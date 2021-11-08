b304e662452406cf2b3fdb2d5516ac96d546c828
LinkMlgElf <W8erhof@gmail.com>
2021-11-08 12:17:03 +0100

    Started working on Hourglass.cs

---
+++ Items/Hourglass.cs
@@ -0,0 +1,24 @@
+﻿using IL.Terraria.GameContent.UI;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OcriumT.Items
+{
+    public class Hourglass : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Stationary Hourglass");
+            Tooltip.SetDefault("Puts you in Stasis for a short time.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.value = 10000;
+            item.consumable = false;
+            item.useTime = 100;
+            item.maxStack = 1;
+
+        }
+    }
+}
