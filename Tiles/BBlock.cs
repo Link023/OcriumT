94be6f0674ddf319c67cc02880e93609ade38360
MisterCavespider <cavespider12@gmail.com>
2022-01-01 22:43:29 +0100

    :sparkles: Added the B Block

---
+++ Tiles/BBlock.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using TileObjectData = Terraria.ObjectData.TileObjectData;
+
+namespace OcriumT.Tiles
+{
+    public class BBlock : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = false;
+            Main.tileBlockLight[Type] = true;
+            Main.tileLighted[Type] = false;
+            drop = ModContent.ItemType<Items.Placeable.BBlock>();
+            AddMapEntry(new Color(255, 0, 0));
+
+            Main.tileFrameImportant[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.newTile.CoordinatePadding = 1;
+            TileObjectData.newTile.AnchorBottom = AnchorData.Empty;
+            TileObjectData.addTile(Type);
+        }
+    }
+}
