8ffd04f2f0ec965b791f7920f4f1bd1b8be67288
MisterCavespider <cavespider12@gmail.com>
2022-01-04 14:29:02 +0100

    :bento: Added spritesheet for B Block

--- Tiles/BBlock.cs
+++ Tiles/BBlock.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ModLoader;
-using TileObjectData = Terraria.ObjectData.TileObjectData;
 
 namespace OcriumT.Tiles
 {
@@ -16,12 +14,6 @@ namespace OcriumT.Tiles
             Main.tileLighted[Type] = false;
             drop = ModContent.ItemType<Items.Placeable.BBlock>();
             AddMapEntry(new Color(255, 0, 0));
-
-            Main.tileFrameImportant[Type] = true;
-            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
-            TileObjectData.newTile.CoordinatePadding = 1;
-            TileObjectData.newTile.AnchorBottom = AnchorData.Empty;
-            TileObjectData.addTile(Type);
         }
     }
 }
