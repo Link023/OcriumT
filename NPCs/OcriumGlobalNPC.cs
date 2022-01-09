3e9edc7e87e824109e3b31c9b8a0147f16eb4f9c
MisterCavespider <cavespider12@gmail.com>
2022-01-09 18:06:57 +0100

    :art: Re-used the GlobalNPC to modify Zombie loot.

---
+++ NPCs/OcriumGlobalNPC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OcriumT.Items.Weapons;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OcriumT.NPCs
+{
+    public class OcriumGlobalNpc : GlobalNPC
+    {
+        public override void NPCLoot(NPC npc)
+        {
+            // Zombies get a 8 in 30 (=3.75%) chance to drop a soup ladle
+            if (npc.type == NPCID.Zombie && Main.rand.Next(30) < 8)
+            {
+                Item.NewItem(npc.getRect(), ModContent.ItemType<SoupLadle>(), 1);
+            }
+            
+            // Pass
+        }
+    }
+}
