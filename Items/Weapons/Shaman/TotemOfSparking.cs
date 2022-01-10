77d13d0759ca8c6c37fe97370aa99fbd03361049
MisterCavespider <cavespider12@gmail.com>
2022-01-10 15:13:06 +0100

    :construction: Tweaked stats for the Totem of Sparking

--- Items/Weapons/Shaman/TotemOfSparking.cs
+++ Items/Weapons/Shaman/TotemOfSparking.cs
@@ -44,7 +44,7 @@ namespace OcriumT.Items.Weapons.Shaman
 	        item.height = 32;
 	        item.useTime = 36;
 	        item.useAnimation = 36;
-	        item.useStyle = ItemUseStyleID.SwingThrow;
+	        item.useStyle = ItemUseStyleID.HoldingUp;
 	        item.value = Item.buyPrice(0, 30, 0, 0);
 	        item.rare = ItemRarityID.Cyan;
 	        item.UseSound = SoundID.Item44;
@@ -71,11 +71,11 @@ namespace OcriumT.Items.Weapons.Shaman
     public class TotemOfSparkingMinion : ModProjectile
     {
         public override void SetStaticDefaults() {
-			DisplayName.SetDefault("Example Minion");
+			DisplayName.SetDefault("Wand of Sparking");
 			// Sets the amount of frames this minion has on its spritesheet
 			Main.projFrames[projectile.type] = 1;
 			// This is necessary for right-click targeting
-			ProjectileID.Sets.MinionTargettingFeature[projectile.type] = true;
+			// ProjectileID.Sets.MinionTargettingFeature[projectile.type] = true;
 
 			// These below are needed for a minion
 			// Denotes that this projectile is a pet or minion
@@ -87,14 +87,13 @@ namespace OcriumT.Items.Weapons.Shaman
 		}
 
 		public sealed override void SetDefaults() {
-			projectile.width = 20;
-			projectile.height = 20;
-			// Makes the minion go through tiles freely
+			projectile.width = 40;
+			projectile.height = 40;
 			projectile.tileCollide = true;
 
 			// These below are needed for a minion weapon
 			// Only controls if it deals damage to enemies on contact (more on that later)
-			projectile.friendly = true;
+			projectile.friendly = false;
 			// Only determines the damage type
 			projectile.minion = true;
 			// Amount of slots this minion occupies from the total minion slots available to the player (more on that later)
@@ -118,7 +117,7 @@ namespace OcriumT.Items.Weapons.Shaman
 
 			#region Active check
 			// This is the "active check", makes sure the minion is alive while the player is alive, and despawns if not
-			if (player.dead || !player.active) {
+			if (player.dead || !player.active || Vector2.Distance(player.Center, projectile.Center) > 2000f) {
 				player.ClearBuff(ModContent.BuffType<TotemOfSparkingBuff>());
 			}
 			if (player.HasBuff(ModContent.BuffType<TotemOfSparkingBuff>())) {
@@ -126,105 +125,48 @@ namespace OcriumT.Items.Weapons.Shaman
 			}
 			#endregion
 
-			#region General behavior
-			Vector2 idlePosition = player.Center;
-			idlePosition.Y -= 48f; // Go up 48 coordinates (three tiles from the center of the player)
-
-			// If your minion doesn't aimlessly move around when it's idle, you need to "put" it into the line of other summoned minions
-			// The index is projectile.minionPos
-			float minionPositionOffsetX = (10 + projectile.minionPos * 40) * -player.direction;
-			idlePosition.X += minionPositionOffsetX; // Go behind the player
-
-			// All of this code below this line is adapted from Spazmamini code (ID 388, aiStyle 66)
-
-			// Teleport to player if distance is too big
-			Vector2 vectorToIdlePosition = idlePosition - projectile.Center;
-			float distanceToIdlePosition = vectorToIdlePosition.Length();
-			if (Main.myPlayer == player.whoAmI && distanceToIdlePosition > 2000f) {
-				// Whenever you deal with non-regular events that change the behavior or position drastically, make sure to only run the code on the owner of the projectile,
-				// and then set netUpdate to true
-				// projectile.position = idlePosition;
-				// projectile.velocity *= 0.1f;
-				projectile.netUpdate = true;
-			}
-
-			// If your minion is flying, you want to do this independently of any conditions
-			float overlapVelocity = 0.04f;
-			for (int i = 0; i < Main.maxProjectiles; i++) {
-				// Fix overlap with other minions
-				Projectile other = Main.projectile[i];
-				if (i != projectile.whoAmI && other.active && other.owner == projectile.owner && Math.Abs(projectile.position.X - other.position.X) + Math.Abs(projectile.position.Y - other.position.Y) < projectile.width) {
-					// if (projectile.position.X < other.position.X) projectile.velocity.X -= overlapVelocity;
-					// else projectile.velocity.X += overlapVelocity;
-
-					// if (projectile.position.Y < other.position.Y) projectile.velocity.Y -= overlapVelocity;
-					// else projectile.velocity.Y += overlapVelocity;
-				}
-			}
-			#endregion
-
 			#region Find target
 			// Starting search distance
-			Vector2 targetCenter = projectile.position;
+			Vector2 targetCenter = projectile.Center;
 			bool foundTarget = false;
 
 			// This code is required if your minion weapon has the targeting feature
-			if (player.HasMinionAttackTargetNPC) {
-				NPC npc = Main.npc[player.MinionAttackTargetNPC];
-				float between = Vector2.Distance(npc.Center, projectile.Center);
-				// Reasonable distance away so it doesn't target across multiple screens
-				if (between < 2000f) {
-					targetCenter = npc.Center;
-					foundTarget = true;
-				}
-			}
-			if (!foundTarget) {
-				// This code is required either way, used for finding a target
-				for (int i = 0; i < Main.maxNPCs; i++) {
-					NPC npc = Main.npc[i];
-					if (npc.CanBeChasedBy()) {
-						float npcDistance = Vector2.Distance(npc.Center, projectile.Center);
-						bool closest = Vector2.Distance(projectile.Center, targetCenter) > npcDistance;
+			// if (player.HasMinionAttackTargetNPC) {
+			// 	NPC npc = Main.npc[player.MinionAttackTargetNPC];
+			// 	float between = Vector2.Distance(npc.Center, projectile.Center);
+			// 	// Reasonable distance away so it doesn't target across multiple screens
+			// 	if (between < 2000f) {
+			// 		targetCenter = npc.Center;
+			// 		foundTarget = true;
+			// 	}
+			// }
+			// If we have not yet found a player, then find some npc
+			
+			// This code is required either way, used for finding a target
+			for (int i = 0; i < Main.maxNPCs; i++) {
+				NPC npc = Main.npc[i];
+				if (npc.CanBeChasedBy()) {
+					float npcDistance = Vector2.Distance(npc.Center, projectile.Center);
+					bool closest = Vector2.Distance(projectile.Center, targetCenter) > npcDistance;
 
-						if (closest || !foundTarget) {
-							// Additional check for this specific minion behavior, otherwise it will stop attacking once it dashed through an enemy while flying though tiles afterwards
-							// The number depends on various parameters seen in the movement code below. Test different ones out until it works alright
-							bool closeThroughWall = npcDistance < 100f;
-							bool lineOfSight = Collision.CanHitLine(projectile.position, projectile.width, projectile.height, npc.position, npc.width, npc.height);
+					if (closest || !foundTarget) {
+						// Additional check for this specific minion behavior, otherwise it will stop attacking once it dashed through an enemy while flying though tiles afterwards
+						// The number depends on various parameters seen in the movement code below. Test different ones out until it works alright
+						bool closeThroughWall = npcDistance < 100f;
+						bool lineOfSight = Collision.CanHitLine(projectile.Center, projectile.width, projectile.height, npc.position, npc.width, npc.height);
 
-							// if (lineOfSight || closeThroughWall)
-							{
-								targetCenter = npc.Center;
-								foundTarget = true;
-							}
+						if (lineOfSight || closeThroughWall)
+						{
+							targetCenter = npc.Center;
+							foundTarget = true;
 						}
 					}
 				}
 			}
-
-			// friendly needs to be set to true so the minion can deal contact damage
-			// friendly needs to be set to false so it doesn't damage things like target dummies while idling
-			// Both things depend on if it has a target or not, so it's just one assignment here
-			// You don't need this assignment if your minion is shooting things instead of dealing contact damage
-			projectile.friendly = foundTarget;
 			#endregion
 
-			#region Movement
-
-			// Default movement parameters (here for attacking)
-			float speed = 0f;
-			float inertia = 20f;
-
+			#region Attack
 			if (foundTarget) {
-				// Minion has a target: attack (here, fly towards the enemy)
-				// if (Vector2.Distance(projectile.Center, targetCenter) > 40f) {
-				// 	// The immediate range around the target (so it doesn't latch onto it when close)
-				// 	Vector2 direction = targetCenter - projectile.Center;
-				// 	direction.Normalize();
-				// 	direction *= speed;
-				// 	projectile.velocity = (projectile.velocity * (inertia - 1) + direction) / inertia;
-				// }
-				
 				// Minion has a target: attack by shooting it
 				if (projectile.frameCounter % 40 == 0)
 				{
@@ -236,58 +178,17 @@ namespace OcriumT.Items.Weapons.Shaman
 					projectile.frameCounter = 0;
 				}
 			}
-			else {
-				// Minion doesn't have a target: return to player and idle
-				if (distanceToIdlePosition > 600f) {
-					// Speed up the minion if it's away from the player
-					speed = 2f;
-					inertia = 60f;
-				}
-				else {
-					// Slow down the minion if closer to the player
-					speed = 2f;
-					inertia = 80f;
-				}
-				if (distanceToIdlePosition > 20f) {
-					// The immediate range around the player (when it passively floats about)
-
-					// This is a simple movement formula using the two parameters and its desired direction to create a "homing" movement
-					vectorToIdlePosition.Normalize();
-					vectorToIdlePosition *= speed;
-					projectile.velocity = (projectile.velocity * (inertia - 1) + vectorToIdlePosition) / inertia;
-				}
-
-				else if (projectile.velocity == Vector2.Zero) {
-					// If there is a case where it's not moving at all, give it a little "poke"
-					projectile.velocity.X = -0.15f;
-					projectile.velocity.Y = -0.05f;
-				}
-			}
-
-			// projectile.velocity = new Vector2(0f, 2f);
+			#endregion
+			
+			#region Gravity
+			// Fall to the ground
 			float down = projectile.velocity.Y + 2.0f;
-			if (down > 20f) down = 20f;
+			if (down > 8f) down = 8f;
 			projectile.velocity = new Vector2(0f, down);
 			#endregion
 
-			#region Animation and visuals
-			// So it will lean slightly towards the direction it's moving
-			projectile.rotation = projectile.velocity.X * 0.05f;
-
-			// This is a simple "loop through all frames from top to bottom" animation
-			// int frameSpeed = 5;
-			// projectile.frameCounter++;
-			// if (projectile.frameCounter >= frameSpeed) {
-			// 	projectile.frameCounter = 0;
-			// 	projectile.frame++;
-			// 	if (projectile.frame >= Main.projFrames[projectile.type]) {
-			// 		projectile.frame = 0;
-			// 	}
-			// }
+			#region Animation and Visuals
 			projectile.frameCounter++;
-
-			// Some visuals here
-			Lighting.AddLight(projectile.Center, Color.White.ToVector3() * 0.78f);
 			#endregion
 		}
     }
