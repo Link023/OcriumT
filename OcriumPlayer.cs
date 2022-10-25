ecf7f6fd9ab6c19b9e9d99d1979a44af29e7b096
W. Agterhof <W8erhof@gmail.com>
2022-10-25 18:29:48 +0200

    Removed all old .cs files from the 1.3 version, all assets remain. Original code still available on the 1.3 branches. Added a new basic sword called CavespiderSword.cs (artifact from mod generation)

--- OcriumPlayer.cs
+++ OcriumPlayer.cs
@@ -5,32 +5,6 @@ namespace OcriumT
 {
     public class OcriumPlayer : ModPlayer
     {
-        // Do they have space-time displacement?
-        public bool spaceTimeDisplacement;
-        // What's the animation tick?
-        public int spaceTimeDisplacementTick;
-        // How long is an animantion?
-        public static int spaceTimeDisplacementAnim = 24;
-
-        public override void ResetEffects()
-        {
-            spaceTimeDisplacement = false;
-        }
-
-        public override void DrawEffects(PlayerDrawInfo drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright)
-        {
-            // If we have space-time displacement, but the effect is not on yet ...
-            if (spaceTimeDisplacement && !Filters.Scene["SpaceTimeDisplacement"].IsActive())
-            {
-                // ... turn it on!
-                Filters.Scene.Activate("SpaceTimeDisplacement");
-            }
-            // If we do not have space-time displacement, but the effect is still on ...
-            else if(!spaceTimeDisplacement && Filters.Scene["SpaceTimeDisplacement"].IsActive())
-            {
-                // ... turn it off!
-                Filters.Scene.Deactivate("SpaceTimeDisplacement");
-            }
-        }
+        
     }
 }
--- OcriumT.cs
+++ OcriumT.cs
@@ -1,24 +1,8 @@
-using Microsoft.Xna.Framework.Graphics;
-using Terraria;
-using Terraria.Graphics.Effects;
-using Terraria.Graphics.Shaders;
-using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace OcriumT
 {
 	public class OcriumT : Mod
 	{
-		public override void Load()
-		{
-			if (Main.netMode != NetmodeID.Server)
-			{
-				// Load shaders ...
-				Ref<Effect> displacementEffect = new Ref<Effect>(GetEffect("Effects/STD"));
-				Filters.Scene["SpaceTimeDisplacement"] =
-					new Filter(new ScreenShaderData(displacementEffect, "FilterSTD"), EffectPriority.Medium);
-				Filters.Scene["SpaceTimeDisplacement"].Load();
-			}
-		}
 	}
 }
