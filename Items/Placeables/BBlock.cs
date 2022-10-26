340e75ad909f61cbb006421a252936b1cd900055
MisterCavespider <cavespider12@gmail.com>
2022-10-26 10:59:32 -0700

    :truck: Ported B Block from 1.3 branch

---
+++ Items/Placeables/BBlock.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OcriumT.Items.Placeables;
+
+public class BBlock : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        Tooltip.SetDefault("B");
+    }
+
+    public override void SetDefaults()
+    {
+        // Placing
+        Item.autoReuse = true;
+        Item.useAnimation = 15;
+        Item.useTime = 10;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.consumable = true;
+        Item.createTile = ModContent.TileType<Tiles.BBlock>();
+        // Sprite
+        Item.width = 12;
+        Item.height = 12;
+        // Misc
+        Item.maxStack = 999;
+        Item.rare = ItemRarityID.White;
+        Item.value = Item.sellPrice(silver: 1, copper: 50);
+    }
+}
---
+++ Tiles/BBlock.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace OcriumT.Tiles
+{
+    public class BBlock : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = false;
+            Main.tileBlockLight[Type] = true;
+            Main.tileLighted[Type] = false;
+            AddMapEntry(new Color(255, 0, 0));
+        }
+
+        public override bool Drop(int i, int j)
+        {
+            return true;
+        }
+    }
+}
